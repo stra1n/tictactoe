cf03123323a294236f76846db5883b57b43191ee
tri_nguyenm <tri_nguyenm@yahoo.com>
2013-08-07 02:23:45 -0700

    add new files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tictactoe::Application.config.secret_token = 'b7b5735fd4b37e27a3ead33ff23437f41d77ffc2b09565082b1d072528d1d715c2e394d5ead9565115600ce417180b36e511e1b6c9a8ddd6e6256b4a8b695339'
